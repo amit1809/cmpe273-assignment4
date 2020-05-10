7246121a8ffefe53b75da5753cf0f2baf8d19681
amit1809 <amitsharma1809@yahoo.com>
2020-05-10 16:36:36 -0700

    update consistent hashing

--- ch_node_ring.py
+++ ch_node_ring.py
@@ -5,14 +5,20 @@ from pickle_hash import hash_code_hex
 class ChNodeRing():
 
     def __init__(self, server_list, virtual_nodes=32):
+
+        #generate list of nodes and virtual nodes
         nodes = self.generate_nodes(server_list, virtual_nodes)
+
+        #create hash node list and then sort it
         hnodes = [self.hash(node) for node in nodes]
-        hnodes.sort() #sorted hash ring of the nodes
+        hnodes.sort() #sorted hash nodes list (hash ring)
+
         self.virtual_nodes = virtual_nodes
         self.nodes = nodes
         self.hnodes = hnodes
+
+        #keeping hash(node) and node as key:value
         self.nodes_map = {self.hash(node): node.split("-")[1] for node in nodes}
-        #print(self.nodes_map)
 
     #using existing hash function for hasing node and key both
     def hash(self, val):
